648c8d15d2a33c3061fdc5e431699b9cb9e05e85
sgurung183 <gurungsushant30@gmail.com>
2026-03-23 12:06:11 -0700

    initial project setup

---
+++ src/main/java/com/hireready/hireready/HirereadyApplication.java
@@ -0,0 +1,13 @@
+package com.hireready.hireready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirereadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirereadyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hireready/hireready/HirereadyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hireready.hireready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirereadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
